92fedf5dea3e2c8fe23160dcdb1b87f67e2d613d
hhl6833 <hn6833@gmail.com>
2015-10-14 12:01:13 +0800

    red5服务器端调用客户端方法

--- red5-server/src/com/hn6833/Application.java
+++ red5-server/src/com/hn6833/Application.java
@@ -2,12 +2,21 @@ package com.hn6833;
 
 import org.red5.server.adapter.ApplicationAdapter;
 import org.red5.server.api.IConnection;
+import org.red5.server.api.service.IServiceCapableConnection;
 
 
 public class Application extends ApplicationAdapter {
     @Override
     public boolean appConnect(IConnection conn, Object[] params) {
+        callClient(conn);
         return true;
+    }
+
+    private void callClient(IConnection conn) {
+        if (conn instanceof IServiceCapableConnection) {
+            IServiceCapableConnection sc = (IServiceCapableConnection) conn;
+            sc.invoke("clientMethod", new Object[]{"one", 1});
+        }
     }
 
     public String change(String str) {
