f252e59bbc650b7cd2969b48b8d52bba0810b275
hhl6833 <hn6833@gmail.com>
2015-10-14 14:37:55 +0800

    输入日志

--- red5-server/src/com/hn6833/Application.java
+++ red5-server/src/com/hn6833/Application.java
@@ -3,11 +3,17 @@ package com.hn6833;
 import org.red5.server.adapter.ApplicationAdapter;
 import org.red5.server.api.IConnection;
 import org.red5.server.api.service.IServiceCapableConnection;
+import org.red5.logging.Red5LoggerFactory;
+import org.slf4j.Logger;
 
 
 public class Application extends ApplicationAdapter {
+    private static Logger logger = Red5LoggerFactory.getLogger(Application.class);
+
     @Override
     public boolean appConnect(IConnection conn, Object[] params) {
+        logger.info("create connection");
+        logger.debug("create connection");
         callClient(conn);
         return true;
     }
