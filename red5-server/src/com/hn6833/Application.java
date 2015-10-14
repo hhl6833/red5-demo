f13b1e00fd097b5614b6551fa157a417c6455795
hhl6833 <hn6833@gmail.com>
2015-10-14 16:25:11 +0800

    遍历所有连接到服务端的用户

--- red5-server/src/com/hn6833/Application.java
+++ red5-server/src/com/hn6833/Application.java
@@ -2,31 +2,36 @@ package com.hn6833;
 
 import org.red5.server.adapter.ApplicationAdapter;
 import org.red5.server.api.IConnection;
+import org.red5.server.api.scope.IScope;
+import org.red5.server.api.service.IPendingServiceCall;
+import org.red5.server.api.service.IPendingServiceCallback;
 import org.red5.server.api.service.IServiceCapableConnection;
-import org.red5.logging.Red5LoggerFactory;
-import org.slf4j.Logger;
 
+import java.util.Set;
 
-public class Application extends ApplicationAdapter {
-    private static Logger logger = Red5LoggerFactory.getLogger(Application.class);
-
+public class Application extends ApplicationAdapter implements IPendingServiceCallback {
     @Override
     public boolean appConnect(IConnection conn, Object[] params) {
-        logger.info("create connection");
-        logger.debug("create connection");
-        callClient(conn);
+        this.callEvery(conn.getScope());
         return true;
+    }
+
+    private void callEvery(IScope scope) {
+        for (Set<IConnection> set : scope.getConnections()) {
+            for (IConnection iConnection : set) {
+                this.callClient(iConnection);
+            }
+        }
     }
 
     private void callClient(IConnection conn) {
         if (conn instanceof IServiceCapableConnection) {
             IServiceCapableConnection sc = (IServiceCapableConnection) conn;
-            sc.invoke("clientMethod", new Object[]{"one", 1});
+            sc.invoke("clientMethod", new Object[]{conn.getSessionId(), 1}, this);
         }
     }
 
-    public String change(String str) {
-        return str.toUpperCase();
+    public void resultReceived(IPendingServiceCall arg0) {
+        log.info("from client:" + arg0.getResult());
     }
-
 }
